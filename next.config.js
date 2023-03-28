a8084a3e227bdce6d394038d1ae20ae33d80a981
오유진 <omh232323@gmail.com>
2023-03-28 15:34:00 +0900

    1234

--- next.config.js
+++ next.config.js
@@ -3,22 +3,24 @@
 const API_KEY = process.env.API_KEY;
 
 const nextConfig = {
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/movies",
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`,
-      },
-      {
-        source: "/api/movies/:id",
-        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&language=ko-KR`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/movies",
+  //       destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`,
+  //     },
+  //     {
+  //       source: "/api/movies/:id",
+  //       destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}&language=ko-KR`,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
